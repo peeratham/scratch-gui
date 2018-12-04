1c5aa3533cc189f1ff29b140fa2c2c5c0d9a17d6
Peeratham Techapalokul <tpeera4@vt.edu>
2018-12-04 14:09:10 -0500

    change content of deck

--- src/lib/libraries/improve-decks/index.jsx
+++ src/lib/libraries/improve-decks/index.jsx
@@ -1,44 +1,61 @@
 import React from 'react';
-import {FormattedMessage} from 'react-intl';
+import { FormattedMessage } from 'react-intl';
 import libraryIntro from './images/lib-getting-started.jpg';
 import stepMove from './images/intro1.gif';
 import stepMoveSayHello from './images/intro2.gif';
 
 
 export default {
-    'intro-move-sayhello': {
+    'duplicate-expr': {
         name: (
             <FormattedMessage
-                defaultMessage="Hello World"
-                description="Name for the 'Getting Started' how-to"
-                id="gui.howtos.intro-move-sayhello-hat.name"
+                defaultMessage="Reusable Repeats: Expressions"
+                description="Name for the 'How to Reuse Data' how-to"
+                id="gui.howtos.duplicate-expr.name"
             />
         ),
-        tags: ['help', 'stuck', 'how', 'can', 'say'],
+        tags: ['help', 'duplication', 'clone', 'how', 'can', 'expression'],
         img: libraryIntro,
-        steps: [{
-            video: 'intro-move-sayhello'
-        }, {
+        steps: [ {
             title: (
                 <FormattedMessage
-                    defaultMessage="Add a move block"
-                    description="Step name for 'Add a move block' step"
-                    id="tips"
+                    defaultMessage="You can reuse recurring block expressions using a variable!"
+                    description=""
+                    id="gui.howtos.duplicate-expr.overview"
                 />
             ),
             image: stepMove
-        }, {
+        },   {
             title: (
                 <FormattedMessage
-                    defaultMessage="Click the green flag to start"
-                    description="Step name for 'Add A Say Block' step"
-                    id="gui.howtos.add-a-move-block.step_stepMoveSayHello"
+                    defaultMessage="Right click the light bulb icon and click on 'Reuse the value'"
+                    description=""
+                    id="gui.howtos.duplicate-expr.step_click-light-bulb"
+                />
+            ),
+            image: stepMove
+        },  {
+            title: (
+                <FormattedMessage
+                    defaultMessage="Give a meaningful name for the variable,"
+                    description=""
+                    id="gui.howtos.duplicate-expr.step_provide-var-name"
+                />
+            ),
+            image: stepMove
+        },
+        {
+            title: (
+                <FormattedMessage
+                    defaultMessage="Nicely done! You have just removed duplicate expressions!"
+                    description=""
+                    id="gui.howtos.add-a-move-block.step_done"
                 />
             ),
             image: stepMoveSayHello
         }, {
             deckIds: [
-             
+
             ]
         }
         ],

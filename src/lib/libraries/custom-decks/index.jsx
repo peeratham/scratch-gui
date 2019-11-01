db625285d233bde5559804be83a46a25dd1ec876
Peeratham Techapalokul <tpeera4@vt.edu>
2019-10-31 23:18:03 -0400

    slide right instruction

--- src/lib/libraries/custom-decks/index.jsx
+++ src/lib/libraries/custom-decks/index.jsx
@@ -25,6 +25,8 @@ import jumpDownward from './intro/jump-downward-code.png';
 import makeABlockResult from './intro/make-a-block-result.png';
 import jumpBlock from './intro/jump-block.png';
 import defineJump from './intro/define-jump.png';
+import extractSlideRight from './intro/extract-slide-right.gif';
+import renameToSlideRight from './intro/rename-to-slide-right.gif';
 import orangeBlue from './intro/orange-blue.gif';
 import orangeBlueStarter from './intro/orange-blue-starter.gif';
 import orangeBlueModifyTarget from './intro/orange-blue-modify-target-2.png';
@@ -250,32 +252,33 @@ export default {
             //     ),
             //     shouldCleanup: true
             // },
-            {
-                id: 'sliding-square',
-                title: (<p className={styles.contentTitle}><PracticeLabel />Go Square Go!</p>),
-                content: (
-                    <div className={styles.contentContainer}>
-                        <p>
-                            <img src={slidingSquareOutput1} className={styles.contentImage} style={{ width: '220px', float:'right' }} />
-                            Program the the blue square to slide through all checkpoints. The result should look like the animation on the right. You will add to the script that starts with <b>when I receive "Level 1 Start"</b> Each slide animation is a little pause (0.1 seconds) followed by a 100-step movement in a given direction.
-                            {/* <img src={goSquare} className={styles.block} style={{ width: '10rem', height: 'auto' }} />. */}
-                        </p>
-                        <p><StepLabel step="Step 0/3" />
-                            (Already completed for you as an example!) Please, click <GreenFlagButton/> to see the example. We move the square rightward to the first checkpoint. If you look at the grid lines in the background, the first checkpoint is 100 steps to the right of the starting point. We <b>repeat</b> the <b>"change x by 10"</b> block <b>10</b> times (i.e., 10 x 10 = 100).
-                        </p>
-                        <p>
-                            <StepLabel step="Step 1/3" />Move the square upward to the second checkpoint which is 100 steps above the first one. Following what we did for the first checkpoint, using the combination of repeat block but this time, use the <b>"change y by 10"</b> block instead.
-                        </p>
-                        <p>
-                            <StepLabel step="Step 2/3" />Move the square rightward to the third check point, similar to step 0/3.
-                        </p>
-                        <p>
-                            <StepLabel step="Step 3/3" />Move the square upward to the final checkpoint, similar to Step 1/3
-                        </p>
-                    </div>
-                ),
-                projectId: 341014499
-            },
+            // {
+            //     id: 'sliding-square',
+            //     title: (<p className={styles.contentTitle}><PracticeLabel />Go Square Go!</p>),
+            //     content: (
+            //         <div className={styles.contentContainer}>
+            //             <p>
+            //                 <img src={slidingSquareOutput1} className={styles.contentImage} style={{ width: '220px', float:'right' }} />
+            //                 Program the the blue square to slide through all checkpoints. The result should look like the animation on the right. You will add to the script that starts with <b>when I receive "Level 1 Start".</b> 
+            //                 <span className={styles.yellowMark}>Each slide animation is a little pause (0.1 seconds) followed by a 100-step movement in a given direction.</span>
+            //                 {/* <img src={goSquare} className={styles.block} style={{ width: '10rem', height: 'auto' }} />. */}
+            //             </p>
+            //             <p><StepLabel step="Step 0/3" />
+            //                 (Already completed for you as an example!) Please, click <GreenFlagButton/> to see the example. We move the square rightward to the first checkpoint. If you look at the grid lines in the background, the first checkpoint is 100 steps to the right of the starting point. We <b>repeat</b> the <b>"change x by 10"</b> block <b>10</b> times (i.e., 10 x 10 = 100).
+            //             </p>
+            //             <p>
+            //                 <StepLabel step="Step 1/3" />Move the square upward to the second checkpoint which is 100 steps above the first one. Following what we did for the first checkpoint, using the combination of repeat block but this time, use the <b>"change y by 10"</b> block instead.
+            //             </p>
+            //             <p>
+            //                 <StepLabel step="Step 2/3" />Move the square rightward to the third check point, similar to step 0/3.
+            //             </p>
+            //             <p>
+            //                 <StepLabel step="Step 3/3" />Move the square upward to the final checkpoint, similar to Step 1/3
+            //             </p>
+            //         </div>
+            //     ),
+            //     projectId: 341014499
+            // },
             {
                 id: 'my-sliding-block',
                 title: (<p className={styles.contentTitle}><PracticeLabel />Making Your Own Slide Blocks!</p>),
@@ -284,16 +287,27 @@ export default {
                         <p>
                             Your program works great and we can improve it further! In the long run, it pays off if you improve your program along the way. You might notice two recurring block sequences.
                             The first recurring block sequence performs slide right and the other slide up. 
-                            We can extract these two recurring block sequences as custom blocks. The steps below guide you how:
+                            You can extract these two recurring block sequences as custom blocks to make your program easy to understand and modify. The steps below will guide you:
                         </p>
                         <p><StepLabel step="Step 0/2" />
-                            Toggle on <b>Code Wizard</b> to see code improvement hints:
+                            {/* Toggle on <b>Code Wizard</b> to see code improvement hints: */}
+                            Toggle <b>Code Wizard</b> on <img src={featureTogglingImg} className={styles.imgInline} style={{width:'180px'}} />
+                                to see improvement hints and follow their suggestions.
                         </p>
                         <p><StepLabel step="Step 1/2" />
-                            Create "Slide Right" custom block.
+                            A hint icon should show up next to the recurring block sequence. 
+                            Click the hint icon and adjust the selection so it covers the recurring block sequences that do the slide right animation and then click Extract.
+                            <img src={extractSlideRight} className={styles.contentImage} style={{ height: '240px', width:'auto' }} /><br/>
+                            Rename the extracted custom block to "Slide Right".
+                            <img src={renameToSlideRight} className={styles.contentImage} style={{ height: '240px', width:'auto' }} />
+                            
                         </p>
                         <p><StepLabel step="Step 2/2" />
-                            Create "Slide Up" custom block.
+                            Follow the similar steps in Step 1/2 to extract a "Slide Up" custom block.
+                        </p>
+                        <p><StepLabel step="Finally" />
+                            Click <GreenFlagButton/> to see the result! Everything should work like before!
+                            Click <CheckButtonImg/> to check your work.
                         </p>
                     </div>
                 ),
